51a75bc52bd698d2c7bab9b32217c4d4d68a7eb0
Huy-DNA <huydo862003@gmail.com>
2024-09-15 23:36:56 +0700

    chore: config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: './src/index.ts',
       name: 'index',
       fileName: 'index',
       formats: ['cjs', 'es'],
