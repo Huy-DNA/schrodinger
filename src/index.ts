ca34b50be835685a9c1b033da426845cc676736b
Huy-DNA <huydo862003@gmail.com>
2024-09-16 07:28:03 +0700

    feat: get, set, clear

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+export class Catche<K, V extends object> {
+  #map: Map<K, WeakRef<V>>;
+  #registry: FinalizationRegistry<K>;
+  constructor () {
+    const map = new Map();
+    this.#map = map;
+    this.#registry = new FinalizationRegistry((key) => {
+      map.delete(key);
+    });
+  }
+
+  get (key: K): V | undefined {
+    return this.#map.get(key)?.deref();
+  }
+
+  set (key: K, value: V) {
+    const oldVal = this.#map.get(key)?.deref();
+    if (oldVal) {
+      this.#registry.unregister(oldVal);
+    }
+    this.#map.set(key, new WeakRef(value));
+    this.#registry.register(value, key, value);
+  }
+
+  clear () {
+    this.#map.clear();
+    const newMap = new Map();
+    this.#map = newMap;
+    this.#registry = new FinalizationRegistry((key) => {
+      newMap.delete(key);
+    });
+  }
+}
